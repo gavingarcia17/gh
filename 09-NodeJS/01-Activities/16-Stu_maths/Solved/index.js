27a46963a7615747112fd367fec0190f272c7102
Logan Garland <legarland@gmail.com>
2023-08-22 21:32:42 -0700

    Day1&2 solutions

---
+++ 09-NodeJS/01-Activities/16-Stu_maths/Solved/index.js
@@ -0,0 +1,30 @@
+const maths = require('./maths');
+
+const operation = process.argv[2];
+
+const numOne = parseInt(process.argv[3]);
+const numTwo = parseInt(process.argv[4]);
+
+console.log(maths.flag);
+
+if (isNaN(numOne) || isNaN(numTwo)) {
+  console.log("oops");
+  return;
+}
+
+switch (operation) {
+  case 'sum':
+    console.log(maths.sum(numOne, numTwo));
+    break;
+  case 'difference':
+    console.log(maths.difference(numOne, numTwo));
+    break;
+  case 'product':
+    console.log(maths.product(numOne, numTwo));
+    break;
+  case 'quotient':
+    console.log(maths.quotient(numOne, numTwo));
+    break;
+  default:
+    console.log('Check your maths!');
+}
